4cd15383f9aa87507c87ea4aad904afe4abefd1a
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2024-06-16 20:24:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.getPrimes(n);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+// import java.util.ArrayList;
+
+class Solution {
+        public static boolean isPrime(long n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (long i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    public static ArrayList<Integer> getPrimes(int n) {
+        for (long i = 2; i <= n / 2; i++) {
+            if (isPrime(i) && isPrime(n - i)) {
+                ArrayList<Integer> result = new ArrayList<>();
+                result.add((int)i);
+                result.add(n - (int)i);
+                return result;
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(-1);
+        result.add(-1);
+        return result;
+    }
+}
