aa5c196280397723e7d14b7cd392c5565487032e
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2025-02-15 18:37:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    String removeDuplicates(String s) {
+        HashSet<Character> hash = new HashSet<>();
+        String newS = "";
+        for(char i : s.toCharArray()){
+            if(!hash.contains(i)){
+                newS+=i;
+            }
+                hash.add(i);
+        }
+        return newS;
+    }
+}
