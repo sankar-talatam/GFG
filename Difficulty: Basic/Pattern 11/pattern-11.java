630a3004a01a4a11dd634f1de3995506ad4e4d43
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2025-03-07 20:41:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 11/pattern-11.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for(int i = 1 ; i <= n ; i++){
+            int start = i%2==0 ? 0 : 1;
+            for(int j = 0 ; j < i ; j++){
+                System.out.print(start+" ");
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+}
