44d108a3f8254cf234b5776f1d1a3cf50886715f
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2025-06-06 22:03:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,44 +1,30 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-// User function Template for Java
-
 class Solution {
     public int getSecondLargest(int[] arr) {
-        // Code Here
+        // code here
+        // code 
         Arrays.sort(arr);
-        if(arr.length<2) return -1;
-        LinkedHashSet<Integer> hs = new LinkedHashSet<>();
-        for(int i : arr){
-            hs.add(i);
+        int now=0;
+        int prev=0;
+        int temp=0;
+        int count=0;
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]<arr[i+1] && arr[i]!=arr[i+1])
+            {
+                now=arr[i];
+                temp=now;
+            }
+            prev=temp;
+            if(arr[i]==arr[i+1])
+                {
+                    count++;
+                    //System.out.println(count);
+                }
         }
-        ArrayList<Integer> ar = new ArrayList<>(hs);
-        if(ar.size()==1) return -1;
-        return ar.get(ar.size()-2);
+        
+        if(count==arr.length-1)
+        return -1;
+        else
+        return prev;
     }
 }
