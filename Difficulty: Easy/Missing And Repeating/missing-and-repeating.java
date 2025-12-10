9fd3586096ac08d295d98ee9d6408d246314bc0e
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2025-12-10 22:14:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,32 +1,31 @@
-// User function Template for Java
-
 class Solution {
-    // Function to find two elements in array
     ArrayList<Integer> findTwoElement(int arr[]) {
         // code here
-        ArrayList<Integer> arList = new ArrayList<>();
-       HashMap<Integer,Integer> hmap = new HashMap<>();
-       Arrays.sort(arr);
-       int c = 0;
-       int[] newArr = new int[2];
-       int n = arr.length;
-       for(int i = 1 ; i <= n ; i++ ){
-           hmap.put(arr[i-1],hmap.getOrDefault(arr[i-1],0)+1);
-       }
-       for(int i = 1 ; i <= n ; i++){
-           if(hmap.getOrDefault(i,0) == 0){
-               newArr[1] = i;
-               c++;
-           }
-           else if(hmap.getOrDefault(i,0) == 2){
-               newArr[0] = i;
-               c++;
-           }
-           if(c == 2) break;
-       }
-       for(int i : newArr){
-           arList.add(i);
-       }
-       return arList;
+        int n = arr.length;
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            hmap.put(arr[i] , hmap.getOrDefault(arr[i],0) + 1);
+        }
+        
+        ArrayList<Integer> arlist = new ArrayList<>();
+        int mis = -1;
+        int rep = -1;
+        
+        for(int i = 1; i <=n; i++){
+            if(hmap.get(i) == null){
+                mis = i;
+            }
+            else if(hmap.get(i) == 2){
+                rep = i;
+            }
+            if(mis != -1 && rep != -1) break;
+        }
+        
+        arlist.add(rep);
+        arlist.add(mis);
+        
+        return arlist;
+        
     }
 }
