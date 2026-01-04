0dfd68462363d52036b74c3730f4ecf37becc83b
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2026-01-04 22:29:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,7 +1,25 @@
 class Solution {
     public void sort012(int[] arr) {
         // code here
-        Arrays.sort(arr);
+       int count0 = 0;
+       int count1 = 0;
+       int count2 = 0;
         
+        for(int i : arr){
+            if(i == 0) count0++;
+            else if(i == 1) count1++;
+            else count2++;
+        }
+        
+        int idx = 0;
+        for(int i = 0; i < count0; i++){
+            arr[idx++] = 0;
+        }
+        for(int i = 0; i < count1; i++){
+            arr[idx++] = 1;
+        }
+        for(int i = 0; i < count2; i++){
+            arr[idx++] = 2;
+        }
     }
 }
