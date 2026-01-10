b7e18fa6c4482dc465f0cf8c975ea48d72514f08
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2026-01-10 23:42:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decimal to any base conversion/decimal-to-any-base-conversion.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    static String getNumber(int B, int N) {
+        // code here
+        String bin = Integer.toString(N,B).toUpperCase();
+        return bin;
+    }
+}
