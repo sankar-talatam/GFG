738f919f2a0247551fa8a737577d932eb89f4559
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2026-01-24 22:47:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking input - Java/taking-input-java.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        // Take all inputs
+        // Print all the values
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        double b = sc.nextDouble();
+        long c = sc.nextLong();
+        byte d = sc.nextByte();
+        sc.nextLine();
+        String s = sc.nextLine();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+    }
+}
