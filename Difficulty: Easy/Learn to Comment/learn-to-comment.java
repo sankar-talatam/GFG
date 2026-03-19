7c70efe28ac12ecbfd7c29cdab0fa462593aeecb
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2026-03-19 23:31:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.java
@@ -0,0 +1,7 @@
+class Solution {
+    public void printValues(int a, int b, int c) {
+        System.out.print(a + " ");
+        // System.out.print(b + " ");
+        System.out.print(c + " ");
+    }
+}
