b992f6089760b10234a78591b5b37450ebf18bed
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2026-05-26 00:00:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkElements(int start, int end, int[] arr) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : arr){
+            set.add(num);
+        }
+        
+        for(int i = start; i <= end; i++){
+            if(!set.contains(i)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
