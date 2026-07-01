ee870edb8e71f4cee9e7282dc8ecafc9fe302103
Sankar Talatam <119839082+sankar-talatam@users.noreply.github.com>
2026-07-01 23:01:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Square/print-square.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i = 0; i < n; i++){
+            if(i == 0 || i == n - 1){
+                for(int j = 0; j < n; j++){
+                    System.out.print("*" + " ");
+                }
+            }
+            else{
+                for(int j = 0; j < n; j++){
+                    if(j == 0 || j == n - 1)
+                        System.out.print("*" + " ");
+                    else{
+                        System.out.print( " " +" ");
+                    }
+                }
+            }
+            
+              System.out.println();
+        }
+        
+        
+        
+    }
+}
